a497fe0ee580aa6edb7e02d6449077294a87c9da
Abdelaziz Megdiche <095rn6mk@duck.com>
2024-05-02 04:51:47 +0200

    minor corrections + comparing samples to analytical solution

--- ising5x5.py
+++ ising5x5.py
@@ -38,58 +38,58 @@ def capacite_thermique(energies):
     return np.var(energies)*(temperature_inverse**2)/energies.size
 def susceptibilite(magnetisations):
     return np.var(magnetisations)*(temperature_inverse)/magnetisations.size
-#main
-energies, magnetisations = metropolis_hastings(etat_initial)
-#find and plot heat capacity and suceptibility for different temperatures, in this case, for different values of beta, and different values of magnetic field and interaction energy
-temperatures_inverses = np.linspace(0.01, 1.0, 100)
-capacites_thermiques = np.zeros(temperatures_inverses.size)
-susceptibilites = np.zeros(temperatures_inverses.size)
-for i, temperature_inverse in enumerate(temperatures_inverses):
+if __name__ == '__main__':
     energies, magnetisations = metropolis_hastings(etat_initial)
-    capacites_thermiques[i] = capacite_thermique(energies)
-    susceptibilites[i] = susceptibilite(magnetisations)
-plt.plot(temperatures_inverses, capacites_thermiques, color='IndianRed')
-plt.xlabel('Temperature inverse')
-plt.ylabel('Capacite thermique')
-plt.show()
-plt.plot(temperatures_inverses, susceptibilites, color='RoyalBlue')
-plt.xlabel('Temperature inverse')
-plt.ylabel('Susceptibilite')
-plt.show()
-#same thing for different values of magnetic field
-#reset inverse temperature
-temperature_inverse = 0.10
-champs_magnetiques = np.linspace(-1.0, 1.0, 100)
-capacites_thermiques = np.zeros(champs_magnetiques.size)
-susceptibilites = np.zeros(champs_magnetiques.size)
-for i, champ_magnetique in enumerate(champs_magnetiques):
-    energies, magnetisations = metropolis_hastings(etat_initial)
-    capacites_thermiques[i] = capacite_thermique(energies)
-    susceptibilites[i] = susceptibilite(magnetisations)
-plt.plot(champs_magnetiques, capacites_thermiques, color='IndianRed')
-plt.xlabel('Champ magnetique')
-plt.ylabel('Capacite thermique')
-plt.show()
-plt.plot(champs_magnetiques, susceptibilites, color='RoyalBlue')
-plt.xlabel('Champ magnetique')
-plt.ylabel('Susceptibilite')
-plt.show()
+    #find and plot heat capacity and suceptibility for different temperatures, in this case, for different values of beta, and different values of magnetic field and interaction energy
+    temperatures_inverses = np.linspace(0.01, 1.0, 100)
+    capacites_thermiques = np.zeros(temperatures_inverses.size)
+    susceptibilites = np.zeros(temperatures_inverses.size)
+    for i, temperature_inverse in enumerate(temperatures_inverses):
+        energies, magnetisations = metropolis_hastings(etat_initial)
+        capacites_thermiques[i] = capacite_thermique(energies)
+        susceptibilites[i] = susceptibilite(magnetisations)
+    plt.plot(temperatures_inverses, capacites_thermiques, color='IndianRed')
+    plt.xlabel('Temperature inverse')
+    plt.ylabel('Capacite thermique')
+    plt.show()
+    plt.plot(temperatures_inverses, susceptibilites, color='RoyalBlue')
+    plt.xlabel('Temperature inverse')
+    plt.ylabel('Susceptibilite')
+    plt.show()
+    #same thing for different values of magnetic field
+    #reset inverse temperature
+    temperature_inverse = 0.10
+    champs_magnetiques = np.linspace(-1.0, 1.0, 100)
+    capacites_thermiques = np.zeros(champs_magnetiques.size)
+    susceptibilites = np.zeros(champs_magnetiques.size)
+    for i, champ_magnetique in enumerate(champs_magnetiques):
+        energies, magnetisations = metropolis_hastings(etat_initial)
+        capacites_thermiques[i] = capacite_thermique(energies)
+        susceptibilites[i] = susceptibilite(magnetisations)
+    plt.plot(champs_magnetiques, capacites_thermiques, color='IndianRed')
+    plt.xlabel('Champ magnetique')
+    plt.ylabel('Capacite thermique')
+    plt.show()
+    plt.plot(champs_magnetiques, susceptibilites, color='RoyalBlue')
+    plt.xlabel('Champ magnetique')
+    plt.ylabel('Susceptibilite')
+    plt.show()
 
-#same thing for different values of interaction energy
-#reset magnetic field
-champ_magnetique = -0.8
-energies_interaction = np.linspace(0.1, 2.0, 100)
-capacites_thermiques = np.zeros(energies_interaction.size)
-susceptibilites = np.zeros(energies_interaction.size)
-for i, energie_interaction in enumerate(energies_interaction):
-    energies, magnetisations = metropolis_hastings(etat_initial)
-    capacites_thermiques[i] = capacite_thermique(energies)
-    susceptibilites[i] = susceptibilite(magnetisations)
-plt.plot(energies_interaction, capacites_thermiques, color='IndianRed')
-plt.xlabel('Energie d\'interaction')
-plt.ylabel('Capacite thermique')
-plt.show()
-plt.plot(energies_interaction, susceptibilites, color='RoyalBlue')
-plt.xlabel('Energie d\'interaction')
-plt.ylabel('Susceptibilite')
-plt.show()
+    #same thing for different values of interaction energy
+    #reset magnetic field
+    champ_magnetique = -0.8
+    energies_interaction = np.linspace(0.1, 2.0, 100)
+    capacites_thermiques = np.zeros(energies_interaction.size)
+    susceptibilites = np.zeros(energies_interaction.size)
+    for i, energie_interaction in enumerate(energies_interaction):
+        energies, magnetisations = metropolis_hastings(etat_initial)
+        capacites_thermiques[i] = capacite_thermique(energies)
+        susceptibilites[i] = susceptibilite(magnetisations)
+    plt.plot(energies_interaction, capacites_thermiques, color='IndianRed')
+    plt.xlabel('Energie d\'interaction')
+    plt.ylabel('Capacite thermique')
+    plt.show()
+    plt.plot(energies_interaction, susceptibilites, color='RoyalBlue')
+    plt.xlabel('Energie d\'interaction')
+    plt.ylabel('Susceptibilite')
+    plt.show()
--- isingSimulator.py
+++ isingSimulator.py
@@ -2,12 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #constantes
-nb_iterations = 1000
+nb_iterations = 2000
 variance_proposition = 0.01
 etat_initial = np.array([0, 0, 0])
 matrice_transition = np.array([[0.065, 0.036, -0.056], [0.036, 0.182, -0.078], [-0.056, -0.078, 0.110]])
 echantillons = np.zeros((nb_iterations, 3))
-
+inverse_matrice = np.linalg.inv(matrice_transition)
 #fonctions d'échantillonage
 def proposition_stationnaire():
     return np.random.multivariate_normal(np.zeros(3), variance_proposition*np.eye(3))
@@ -16,7 +16,7 @@ def proposition(etat):
     return etat + proposition_stationnaire()
 
 def distribution_cible(etat):
-    return np.exp(-etat.dot(matrice_transition).dot(etat)/2)
+    return np.exp(-etat.dot(inverse_matrice).dot(etat)/2)
 
 def probabilite_acceptation(etat, nouvel_etat):
     return min(1, distribution_cible(nouvel_etat)/distribution_cible(etat))
@@ -31,18 +31,28 @@ def metropolis_hastings():
         echantillons[i] = etat
 
 #fonctions de tracé
-def tracer_echantillons():
+def tracer_echantillons(comparaison=False):
     fig, axs = plt.subplots(3, 1)
-    axs[0].hist(echantillons[:, 0], bins=30)
+    axs[0].hist(2*echantillons[:, 0], bins=30)
     axs[0].set_title('Coordonnées X')
-    axs[1].hist(echantillons[:, 1], bins=30)
+    axs[1].hist(2*echantillons[:, 1], bins=30)
     axs[1].set_title('Coordonnées Y')
-    axs[2].hist(echantillons[:, 2], bins=30)
+    axs[2].hist(2*echantillons[:, 2], bins=30)
     axs[2].set_title('Coordonnées Z')
+    if comparaison:
+        x = np.linspace(-1, 1, 100)
+        y = np.exp(-x**2/(2*matrice_transition[0, 0]))/np.sqrt(2*np.pi*matrice_transition[0, 0])
+        axs[0].plot(x, y*echantillons.size*2/30, color='red')
+        x= np.linspace(-1, 1, 100)
+        y = np.exp(-x**2/(2*matrice_transition[1, 1]))/np.sqrt(2*np.pi*matrice_transition[1, 1])
+        axs[1].plot(x, y*echantillons.size*2/30, color='green')
+        x= np.linspace(-1, 1, 100)
+        y = np.exp(-x**2/(2*matrice_transition[2, 2]))/np.sqrt(2*np.pi*matrice_transition[2, 2])
+        axs[2].plot(x, y*echantillons.size*2/30, color='blue')
     plt.tight_layout()
     plt.show()
 
-def tracer_vs_echantillons():
+def tracer_vs_echantillons(comparaison=False):
     fig, axs = plt.subplots(3, 1)
     axs[0].scatter(echantillons[:, 0], echantillons[:, 1], label='s1 vs s2')
     axs[0].legend()
@@ -54,7 +64,7 @@ def tracer_vs_echantillons():
     plt.tight_layout()
     plt.show()
 
-def tracer_3D():
+def tracer_3D(comparaison=False):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     ax.scatter(echantillons[:, 0], echantillons[:, 1], echantillons[:, 2])
@@ -62,7 +72,8 @@ def tracer_3D():
 
 #main
 if __name__ == '__main__':
+    comparaison = True
     metropolis_hastings()
-    tracer_echantillons()
-    tracer_vs_echantillons()
-    tracer_3D()
+    tracer_echantillons(comparaison)
+    tracer_vs_echantillons(comparaison)
+    tracer_3D(comparaison)
